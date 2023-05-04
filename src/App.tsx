4d5f4f01defed0fae7289d684a1be7bde018d10e
DLW <guilhermepaterlini@gmail.com>
2023-05-04 00:09:29 -0400

    Criado componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import TaskContainer from "./components/TaskContainer";
+import TaskItem from "./components/TaskItem";
+
 function App() {
+  const listaTarefas = useState([]);
   return (
-  <main className="container">
-    <h1 className="m-5">Task Manaer</h1>
-    <div id="new-task">
-      <button id="btnAdd" className="btn btn-primary">add</button>
-    </div>
-  </main>
-    )
+    <main className="container">
+      <h1 className="m-5">📝 Task Manaer</h1>
+      <div id="new-task">
+        <button id="btnAdd" className="btn btn-primary">
+          add
+        </button>
+      </div>
+      <TaskContainer>
+        <TaskItem titulo="tarefa 1" />
+        <TaskItem titulo="tarefa 2" done={true} />
+      </TaskContainer>
+    </main>
+  );
 }
 
 export default App;
