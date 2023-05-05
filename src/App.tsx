965d7efb4e78c1641186ec9fe41d08b1d78851b5
DLW <guilhermepaterlini@gmail.com>
2023-05-04 20:44:35 -0400

    Feature: adicionado hook de usestate na lista de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,54 @@ import { useState } from "react";
 import TaskContainer from "./components/TaskContainer";
 import TaskItem from "./components/TaskItem";
 
+interface Tarefa {
+  titulo: string;
+  done: boolean;
+}
+
 function App() {
-  const listaTarefas = useState([]);
+  const [tarefas, setTarefas] = useState<Tarefa[]>([]);
+
+  function adicionarTarefa() {
+    setTarefas([
+      ...tarefas,
+      {
+        titulo: "Tarefa 2",
+        done: false,
+      },
+    ]);
+  }
+
+  function concluirTarefa(posicao: number) {
+    const novaLista = [...tarefas];
+
+    novaLista[posicao].done = true;
+
+    setTarefas(novaLista);
+  }
+
   return (
     <main className="container">
-      <h1 className="m-5">📝 Task Manaer</h1>
+      <h1 className="m-5">📝 Task Manager</h1>
       <div id="new-task">
-        <button id="btnAdd" className="btn btn-primary">
+        <button
+          id="btnAdd"
+          className="btn btn-primary"
+          onClick={adicionarTarefa}
+        >
           add
         </button>
       </div>
       <TaskContainer>
-        <TaskItem titulo="tarefa 1" />
-        <TaskItem titulo="tarefa 2" done={true} />
+        {tarefas.map((tarefa: Tarefa, posicao: number) => {
+          return (
+            <TaskItem
+              titulo={tarefa.titulo}
+              done={tarefa.done}
+              concluirTarefa={() => concluirTarefa(posicao)}
+            />
+          );
+        })}
       </TaskContainer>
     </main>
   );
